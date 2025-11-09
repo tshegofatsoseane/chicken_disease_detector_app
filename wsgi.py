48344d7adc40459f9c5398744a4b573d51fc4faa
tshegofatsoseane <tshegofatsoseane98@gmail.com>
2025-11-09 03:58:57 +0200

    production ready

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,4 @@
-from app import create_app
-
-app = create_app()
+from app import app
 
 if __name__ == "__main__":
-    # Optional: for local testing
     app.run(host="0.0.0.0", port=8000, debug=False)
